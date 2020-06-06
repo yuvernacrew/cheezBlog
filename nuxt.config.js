b95fb674221088b10ee148597e00ebdb5ca5cab1
加藤　優奈 <yuvernacrew@gmail.com>
2020-06-07 02:20:49 +0900

    viewportの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,8 @@ export default {
   head: {
     titleTemplate: '%s - cheezBlog',
     meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'ちーずのぶろぐ。' },
       { hid: 'og:site_name', property: 'og:site_name', content: 'cheezBlog' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
